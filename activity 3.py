de139e85156faa0b3d73f8fc4214a8959f2bb3f0
harinivassahana-max <harinivassahana@gmail.com>
2025-11-11 16:26:50 +1100

    activity 3

---
+++ activity 3.py
@@ -0,0 +1,18 @@
+#take input fromthe user
+num = int(input("enter a number:"))
+
+#initialise sum
+sum = 0
+
+#find the sum of the cube of each digit
+temp = num
+while temp>0:
+    digit = temp % 10
+    sum += digit ** 3
+    temp //=10
+
+#display the result
+if num == sum:
+    print(num,"is an armstrong number")
+else:
+    print(num,"is not an armstrong number")
